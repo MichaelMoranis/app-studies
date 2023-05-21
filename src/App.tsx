73c74efa5f86d9e60f3a8b2b3fb207660411910a
aoc <michaelmoranesul@gmail.com>
2023-05-20 23:06:27 -0300

    create list component with <li>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Formulario from './components/Form';
+import List from './components/List';
 
 function App() {
   return (
     <div className="App">
       <Formulario />
+      <List />
     </div>
   );
 }
