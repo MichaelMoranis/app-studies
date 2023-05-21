edca4c18186e95c3037026726ace4f11ab37e585
aoc <michaelmoranesul@gmail.com>
2023-05-20 23:24:44 -0300

    uses the .map list

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 
 function List() {
+  const tarefas = [{
+    tarefa: "React",
+    tempo: "02:00:00"
+  }, {
+    tarefa: "JavaScript",
+    tempo: "01:00:00"
+  }, {
+    tarefa: "TypeScript",
+    tempo: "23:00:00"
+  }];
+
   return (
     <aside>
       <h2>estudos do dia</h2>
       <ul>
-        <li>
-          <h3>
-            React
-          </h3>
-          <span>02:00:00</span>
-        </li>
-        <li>
-          <h3>
-            JavaScript
-          </h3>
-          <span>01:00:00</span>
-        </li>
+         {tarefas.map((item, index) => (
+          <li key={index}>
+            <h3>{item.tarefa}</h3>
+            <span>{item.tempo}</span>
+          </li>
+         ))}
       </ul>
     </aside>
   )
