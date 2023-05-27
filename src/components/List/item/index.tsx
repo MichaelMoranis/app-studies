6776d660a889cf6e1449941d5b03e2271673b886
aoc <michaelmoranesul@gmail.com>
2023-05-27 10:41:10 -0300

    criando metodo de selecionaTarefa e usos de templates strings com onClick para alternar entre tarefas selecionadas ou nao selecionada

--- src/components/List/item/index.tsx
+++ src/components/List/item/index.tsx
@@ -1,10 +1,32 @@
 import { ITarefa } from "../../../types/tarefa";
-import styles from "../list.module.scss"
+import styles from "../list.module.scss";
 
-export default function Item({tarefa, tempo, selecionado, completado, id}: ITarefa) {
-  console.log("item atual: ", {tarefa, tempo, selecionado, completado, id} )
+interface Props extends ITarefa {
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
+}
+
+export default function Item({
+  tarefa,
+  tempo,
+  selecionado,
+  completado,
+  id,
+  selecionaTarefa,
+}: Props) {
+  console.log("item atual: ", { tarefa, tempo, selecionado, completado, id });
   return (
-    <li className={styles.item}>
+    <li
+      className={`${styles.item} ${selecionado ? styles.itemSelecionado : ""}`}
+      onClick={() =>
+        selecionaTarefa({
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id,
+        })
+      }
+    >
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
     </li>
--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -6,12 +6,24 @@ import { useState } from 'react';
 import { ITarefa } from '../../types/tarefa';
 
 function App() {
-  const [tarefas, setTarefas] = useState<ITarefa[] | []>([]
-  );
+  const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+  
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada)
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })))
+  }
+
   return (
     <div className={styles.AppStyle}>
       <Formulario setTarefas={setTarefas} />
-      <List tarefas={tarefas}/>
+      <List 
+      tarefas={tarefas}
+      selecionaTarefa={selecionaTarefa}
+      />
       <Cronometro />
     </div>
   );
